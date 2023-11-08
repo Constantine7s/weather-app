1986c2439f7f34a804d2ab780dee7191760032f1
Konstantin Shambazov <k.shambazov@gmail.com>
2023-11-08 21:36:40 +0900

    add WeatherProvider context for global weather state management

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -1,25 +1,6 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React from 'react';
 import Main from './containers/Main';
-import currentWeatherData from '../../../data/currentWeather.json'
-import weatherForecastData from '../../../data/weatherForecast.json';
-
-const WeatherContext = createContext();
-
-const WeatherProvider = ({ children }) => {
-  const [currentWeather, setCurrentWeather] = useState(null);
-  const [hourlyForecast, setHourlyForecast] = useState(null);
-
-  useEffect(() => {
-    setCurrentWeather(currentWeatherData);
-    setHourlyForecast(weatherForecastData);
-  }, []);
-
-  return (
-    <WeatherContext.Provider value={{ currentWeather, hourlyForecast }}>
-      {children}
-    </WeatherContext.Provider>
-  );
-};
+import WeatherProvider from './context/WeatherProvider';
 
 const App = (props) => {
   return (
---
+++ packages/web/src/context/WeatherProvider.js
@@ -0,0 +1,28 @@
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import currentWeatherData from '../../../../data/currentWeather.json';
+import weatherForecastData from '../../../../data/weatherForecast.json';
+
+const WeatherContext = createContext();
+
+const WeatherProvider = ({ children }) => {
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [hourlyForecast, setHourlyForecast] = useState(null);
+
+  useEffect(() => {
+    setCurrentWeather(currentWeatherData);
+    setHourlyForecast(weatherForecastData);
+}, []);
+
+    return (
+     <WeatherContext.Provider value={{ currentWeather, hourlyForecast }}>
+      {children}
+    </WeatherContext.Provider>
+  );
+};
+
+export const WeatherState = () => {
+  return useContext(WeatherContext);
+};
+
+export default WeatherProvider;
