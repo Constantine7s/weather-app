aae4ab451e3bb69e1c5281bb291fc0512dd08787
Konstantin Shambazov <k.shambazov@gmail.com>
2023-11-09 22:07:36 +0900

    add styling to TemperatureTile with styled components

--- packages/web/src/components/TemperatureTile.js
+++ packages/web/src/components/TemperatureTile.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TileContainer = styled.div`
+  text-align: center;
+  align-items: center;
+  display:flex;
+  flex-direction: column;
+`;
+
+const Title = styled.p`
+  font-size: 1.1rem; 
+  background-color: #1282A2; 
+  color: #ffffff;
+  width: 70px;
+  padding: 6px 8px;
+  border-radius: 20px; 
+  margin: 0;
+`;
+
+const Temperature = styled.p`
+  margin-top: 10px;
+  font-size: 2rem;
+`;
 
 const TemperatureTile = ({ title, temp }) => {
-
   const displayTemp = Math.round(temp);
 
   return (
-    <div>
-      <h3>{title}</h3>
-      <p>{displayTemp}°</p>
-    </div>
+    <TileContainer>
+      <Title>{title}</Title>
+      <Temperature>{displayTemp}°</Temperature>
+    </TileContainer>
   );
 };
-
 
 export default TemperatureTile;
