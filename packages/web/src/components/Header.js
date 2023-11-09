c2c22ffbb20b0751244d37dc50f7f854d5f486b9
Konstantin Shambazov <k.shambazov@gmail.com>
2023-11-09 18:42:36 +0900

    update Header styling using styled components

--- packages/web/src/components/Header.js
+++ packages/web/src/components/Header.js
@@ -1,20 +1,66 @@
 import React from 'react';
-import { WeatherState } from '../context/WeatherProvider'
+import { WeatherState } from '../context/WeatherProvider';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 3rem;
+`;
+
+const Title = styled.h1`
+  font-size: 7rem;
+  font-weight: 500;
+  margin-bottom: 0;
+`;
+
+const WeatherImage = styled.img`
+  width: 150px; 
+  height: 150px; 
+  object-fit: cover;
+`;
+
+const Description = styled.h3`
+  font-size: 3rem;
+  font-weight: 400;
+  margin: 0.5rem 0; 
+`;
+
+const LoadingText = styled.p`
+`;
+
+const SearchIcon = styled.i`
+
+  position: absolute;
+  top: 1rem; 
+  right: 1rem; 
+  font-size: 3rem;
+  cursor: pointer;
+`;
 
 const Header = () => {
   const { currentWeather } = WeatherState();
 
   return (
-    <header>
+    <StyledHeader>
+      <SearchIcon className="fas fa-magnifying-glass-location" /> {/* Font Awesome search icon */}
       {currentWeather ? (
         <>
-          <h1>{currentWeather.name}</h1>
-          <h3>{currentWeather.weather[0].main}</h3>
+          <Title>{currentWeather.name}</Title>
+          <Description>{currentWeather.weather[0].main}</Description>
+          <WeatherImage
+            src={`http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@4x.png`}
+            alt={currentWeather.weather[0].main}
+            title={currentWeather.weather[0].description}
+          />
         </>
       ) : (
-        <p>Loading weather data...</p>
+        <LoadingText>Loading weather data...</LoadingText>
       )}
-    </header>
+    </StyledHeader>
   );
 };
 
